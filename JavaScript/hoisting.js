452b827db85601b3f217000b8ad581d9e41d5d2b
kestsaurav21 <kestsaurav21@outlook.com>
2025-08-22 21:39:37 +0530

    hoisting

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,13 @@
+// Hoisting is a phenomena in JS by which you can access the variable and functions even before initialisation.
+
+// Output based Question on Hoisting
+
+// console.log(y);
+// y=1 
+// Output: ReferenceError: y is not defined
+
+
+// console.log(y);
+// var y=1 
+// Output: undefined
+
